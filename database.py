93912f83aac9d3b1786d0c5f47ed9523eade0b2e
Mensah Luc Germain <89817683+lucgmensah@users.noreply.github.com>
2025-03-22 14:18:55 +0100

    core : Ajout du fichier de configuration de la base de données avec SQLAlchemy

---
+++ database.py
@@ -0,0 +1,23 @@
+# app/database.py
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
+# Pour PostgreSQL, décommentez la ligne suivante:
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/exercices_db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# Fonction pour obtenir une session de base de données
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
